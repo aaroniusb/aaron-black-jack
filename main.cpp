b264bc9f431aa15763492aba984403b79c321721
Aaron Bisping <aaronbisping819@gmail.com>
2026-07-23 02:33:21 -0500

    cleaned a bit up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std ;
 
 //game stuff
@@ -16,22 +17,24 @@ int HCard1;
 int HCard2;
 int HCardSum;
 
+//game state chosing 
+void Selectr(){
+    std::cout << "Play Black Jack: 1" << std::endl;
+    std::cout << "Close program: 2" << std::endl;
+    std::cout << "Choose an option:";
+    cin >> gamestate;
+}
 
 
 int main(){
     gamestate = 0;
     std::cout << "hello welcome to black jack" << std::endl;
 
-    //game state chosing 
-    if(gamestate == 0){
-    std::cout << "play black jack:1" << std::endl;
-    std::cout << "close program:2" << std::endl;
-    std::cout << "choose an option:";
-    cin >> gamestate;
-    }
+    Selectr();    
     
     //playing
-    if (gamestate == 1) {
+    while (gamestate == 1) {
+        std::cout << endl;
         srand(time(0));
         Card1 = rand() % 13 + 1;
         Card2 = rand() % 13 + 1;
@@ -39,22 +42,29 @@ int main(){
         HCard1 = rand() % 13 + 1;
         HCard2 = rand() % 13 + 1;
 
-        std::cout <<"Your cards:" << Card1 << "and " << Card2 << endl;
-        std::cout <<"House's cards:" << HCard1 << "and " << HCard2 << endl;
+        std::cout <<"Your cards:" << Card1 << " and " << Card2 << endl;
+        std::cout <<"House's cards:" << HCard1 << " and " << HCard2 << endl;
 
         CardSum = Card1 + Card2;
         HCardSum = HCard1 + HCard2;
 
         if(CardSum > HCardSum ){
             std::cout <<"YOU WIN!:)" << endl;
-            std::cout <<"Your score:" << CardSum <<"house score:" << HCardSum  << endl;
+            std::cout <<"Your score: " << CardSum <<" house score: " << HCardSum  << endl;
         }
 
         if(CardSum < HCardSum ){
             std::cout <<"You Lose :(" << endl;
-            std::cout <<"Your score:" << CardSum <<"house score:" << HCardSum  << endl;
+            std::cout <<"Your score: " << CardSum <<" house score: " << HCardSum  << endl;
         }
         
+         if(CardSum == HCardSum ){
+            std::cout <<"You Draw :p" << endl;
+            std::cout <<"Your score: " << CardSum <<" house score: " << HCardSum  << endl;
+        }
+
+        std::cout << endl;
+        Selectr();
     }
     
 
@@ -66,4 +76,6 @@ int main(){
 
     return 4;
 }
+
+
 
