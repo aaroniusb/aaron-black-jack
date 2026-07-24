df7584ce123b8ebb321fe1f013cc1607b87833d1
Aaron Bisping <aaronbisping819@gmail.com>
2026-07-24 00:41:24 -0500

    added input verification

--- main.cpp
+++ main.cpp
@@ -22,7 +22,12 @@ void Selectr(){
     std::cout << "Play Black Jack: 1" << std::endl;
     std::cout << "Close program: 2" << std::endl;
     std::cout << "Choose an option:";
-    cin >> gamestate;
+
+    while (!(cin >> gamestate)) {
+        cout << "Invalid input. Choose an option:";
+        cin.clear(); 
+        cin.ignore(255, '\n');
+    }
 }
 
 void CardDraw(){
@@ -34,12 +39,19 @@ void CardDraw(){
     HCard1 = rand() % 13 + 1;
     HCard2 = rand() % 13 + 1;
 
+    if(HCardSum == 21 || CardSum == 21 ){
+        WinCalc();
+    }
+
     std::cout <<"Your cards:" << Card1 << " and " << Card2 << endl;
     std::cout <<"House's cards:" << HCard1 << " and " << HCard2 << endl;
 
 }
 
 void WinCalc(){
+
+    CardSum = Card1 + Card2;
+    HCardSum = HCard1 + HCard2; 
 
     if(CardSum > HCardSum ){
         std::cout <<"YOU WIN!:)" << endl;
@@ -71,9 +83,6 @@ int main(){
 
         CardDraw();
     
-        CardSum = Card1 + Card2;
-        HCardSum = HCard1 + HCard2;
-
         WinCalc();
 
         std::cout << endl;
